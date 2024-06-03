c9b9f07e9e4f5e7ea4c8e77bb6b8bd045fe40d23
Herbert Poul <herbert@codeux.design>
2024-06-03 11:33:07 +0200

    create hooks_async_utils package.

---
+++ packages/hooks_async_utils/lib/src/future_task_hook.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_async_utils/flutter_async_utils.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+AsyncTaskHook useAsyncTask({List<Object>? keys}) {
+  // final asyncTaskHook = useMemoized(() => AsyncTaskHook());
+  // return useListenable(asyncTaskHook);
+  return use(_AsyncTaskHook(keys: keys));
+}
+
+typedef OnError = void Function(BuildContext context, ErrorDetails error);
+
+abstract class AsyncTaskHook {
+  static OnError defaultShowErrorDialog = (context, error) {};
+  VoidCallback? asyncTaskCallback<U>(
+      Future<U> Function(TaskProgress progress) progress);
+  FutureTask? get task;
+
+  OnError? get onError;
+  set onError(OnError? onError);
+}
+
+class _AsyncTaskHook extends Hook<AsyncTaskHook> {
+  const _AsyncTaskHook({super.keys});
+  @override
+  HookState<AsyncTaskHook, Hook<AsyncTaskHook>> createState() =>
+      _AsyncTaskHookState();
+}
+
+class _AsyncTaskHookState extends HookState<AsyncTaskHook, _AsyncTaskHook>
+    implements AsyncTaskHook {
+  late final _manager =
+      FutureTaskManager(showErrorDialog: (ErrorDetails error) {
+    (onError ?? AsyncTaskHook.defaultShowErrorDialog)(context, error);
+  }, onChanged: (FutureTask? task) {
+    setState(() {});
+  });
+
+  @override
+  void initHook() {}
+
+  @override
+  AsyncTaskHook build(BuildContext context) {
+    return this;
+  }
+
+  @override
+  FutureTask? get task => _manager.task;
+
+  @override
+  VoidCallback? asyncTaskCallback<U>(
+      Future<U> Function(TaskProgress progress) progress) {
+    return _manager.asyncTaskCallback(progress);
+  }
+
+  @override
+  OnError? onError;
+}
